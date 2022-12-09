2d5d8643ac3064abac2bc8a00dbf385aca4f4450
AkshayShaji <95688691+AkshayShaji@users.noreply.github.com>
2022-12-09 18:44:50 +0530

    Create Diskscheduling.c

---
+++ Diskscheduling.c
@@ -0,0 +1,24 @@
+include <stdio.h> 
+int main() 
+int n,diff,tst=0,req[20],i; 
+printf("Enter the number of request: "); 
+scanf("%d",&n); 
+printf( "Enter the current head position: "); 
+scanf("%d",&req[0]); 
+printf( "Enter the request: ");
+for(i=1;i<=n;i++)
+{ 
+scanf("%d",&req[i]); 
+if(req[i]<req[i-1])
+{
+diff=req[i-1]-req[i];
+} 
+else
+{
+diff=req[i]-req[i-1];
+} 
+tst=tst+diff ;
+} 
+printf("Total seek time is %d\n",tst); 
+return 0; 
+}
